1dc6f7d04f79742d9cee7d8868b58469d7b824a3
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-03-10 15:25:18 +0000

    addes styling to navbar, renamed js files to index, added images

---
+++ my-portfolio/src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css"
+
+function Header() {
+
+    return(
+        <div className="Header">
+
+           <div class="jumbotron">
+            <h1 class="display-4">Welcome to the fight! </h1>
+            <p class="lead"> ExxtremeBattler <img src="./images/hunter.jpg" alt="ExxtremeBattler's current avatar" ></img>
+      is currently learning: Bootstrap! </p>
+  </div>
+
+</div>
+    )
+}
+
+export default Header;
+
