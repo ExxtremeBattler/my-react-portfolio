7174a7992c4b617061aed7023e15683cf4b3c770
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-03-10 15:13:14 +0000

    initial commit of files, navbar component coded

---
+++ my-portfolio/src/components/Navbar/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./style.css"
+
+function Navbar() {
+
+    return(
+        <div className="Navbar">
+            <nav class="navbar navbar-expand-lg navbar-light bg-red">
+    
+    <a class="navbar-brand" href="#">ExxtremeBattler!</a>
+    
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        
+        <li class="nav-item active">
+          <a class="nav-link" href="#aboutMe"> Skills <span class="sr-only">(current)</span></a>
+        </li>
+        
+        <li class="nav-item active">
+          <a class="nav-link" href="#myWork"> Work <span class="sr-only">(current)</span></a>
+        </li>
+        
+        <li class="nav-item active">
+          <a class="nav-link" href="#contactMe"> About/Contact Me <span class="sr-only">(current)</span></a>
+        </li>
+      
+      </ul>
+      
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> </input>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  
+  </nav>
+        </div>
+    )
+    
+}
