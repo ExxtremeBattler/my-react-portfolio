09c9f30ef13157189245aff49bbde6849eeaff14
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-03-13 13:43:48 +0000

    added about and contact sections

---
+++ my-portfolio/src/components/About/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style.css"
+
+function About() {
+    return (
+        <div className="About">
+            
+            <section class="page-section about" id="aboutMe">
+
+      <h2 class="AM-heading"> Skills </h2>
+
+      <p> <div class="vertical"> An apsiring Front-End Web Developer who loves challenging himself with new projects, skills, and quests. 
+         Proficient in: 
+        - HTML 
+        - CSS 
+        - JavaScript 
+        - Java 
+        - SQL  
+        - React</div> </p>
+      
+    
+    </section> 
+        </div>
+    )
+}
+
+export default About
