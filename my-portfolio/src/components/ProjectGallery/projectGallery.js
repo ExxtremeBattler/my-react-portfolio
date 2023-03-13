ecfe41aef3277baf2a8897190bd2ddd94ca8d881
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-03-13 13:27:44 +0000

    added more html and css to comps

---
+++ my-portfolio/src/components/ProjectGallery/projectGallery.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css"
+
+function ProjectGallery() {
+
+    return(
+        <div className="ProjectGallery">
+
+        <section class="page-section work " id="myWork">
+
+            <h2>Work </h2>
+
+                  <div class="vertical"> </div>
+                          <section id="projectGrid">
+
+</section>  
+</section>
+
+
+</div>
+    )
+}
+
+export default ProjectGallery;
