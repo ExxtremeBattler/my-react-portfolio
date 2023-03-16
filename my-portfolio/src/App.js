1e4aea5b0ccfeba98c94e9ddb3674a22631d3df5
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-03-16 09:35:43 +0000

    projects now render from json

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,23 +1,45 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Project from "./components/Project/project";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import About from "./components/About/index"
 import ProjectGallery from "./components/ProjectGallery/projectGallery"
 import Contact from "./components/Contact";
+import projects from "./projects.json"
 
 function App() {
-  return( <div>
 
-
-    <Navbar></Navbar>
-    <Header />
-    <About />
-    <Project></Project>
-    <ProjectGallery> </ProjectGallery>
-    <Contact></Contact>
+  return( 
   
+  <div>
+
+    <p> {projects[0].title}</p>
+    
+
+    <Project title = {projects[0].title} 
+    image = {projects[0].image}
+    repoLink = {projects[0].repoLink}
+    deployLink = {projects[0].deployLink}/>
+    
   </div>
+
+    // {/* <Project 
+    // title = {projects.title[0]}
+    // image = {projects.image[0]}
+    // deployLink = {projects.deployLink[0]}
+    // repoLink = {projects.repoLink[0]}/> */}
+
+    // {/* <Navbar></Navbar>
+    // <Header />
+    // <About />
+
+    
+
+    // <ProjectGallery> </ProjectGallery>
+    // <Contact></Contact> */}
+  
+  
   
         ) 
 }
--- my-portfolio/src/components/Project/project.js
+++ my-portfolio/src/components/Project/project.js
@@ -3,13 +3,23 @@ import "./style.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function Project() {
+function Project(props) {
+  
+  return (
+    <div id="project">
 
-    return(
-        <div className="project">
-          <p> hi</p>
-</div>
-    )
+          <div class="card">
+            <img src={props.image} class="card-img-top" alt= {props.title} ></img>
+            <div class="card-body">
+              <h5 class="card-title"> {props.title}</h5>
+              <p class="card-text">Deploy Link : {props.deployLink} <br></br>
+              Repo Link : {props.repoLink} </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+          </div>
+  )
 }
+
 
 export default Project;
