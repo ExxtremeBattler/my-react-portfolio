0cbff6637b138d51374616b9deb5e67418fe1b16
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-03-17 13:50:04 +0000

    minor changes to card and navbar

--- my-portfolio/src/components/Navbar/index.js
+++ my-portfolio/src/components/Navbar/index.js
@@ -20,15 +20,15 @@ function Navbar() {
       <ul className="navbar-nav mr-auto">
         
         <li className="nav-item active">
-          <a className="nav-link" href="#aboutMe"> Skills <span className="sr-only">(current)</span></a>
+          <a className="nav-link" href="#aboutMe"> Skills <span className="sr-only"></span></a>
         </li>
         
         <li className="nav-item active">
-          <a className="nav-link" href="#myWork"> Work <span className="sr-only">(current)</span></a>
+          <a className="nav-link" href="#myWork"> Work <span className="sr-only"></span></a>
         </li>
         
         <li className="nav-item active">
-          <a className="nav-link" href="#contactMe"> About/Contact Me <span className="sr-only">(current)</span></a>
+          <a className="nav-link" href="#contactMe"> About/Contact Me <span className="sr-only"></span></a>
         </li>
       
       </ul>
